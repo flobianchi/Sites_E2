2f33f401f2d735b7848de4a07b14b64895c8a44f
Victoria <vacontardo@uc.cl>
2021-05-21 22:08:17 -0400

    toy chata

--- index.php
+++ index.php
@@ -9,34 +9,31 @@
 
   <!-- Consulta 1 -->
   <h3 align="center"> Muestre las direcciones de todas las unidades de la empresa de despachos.</h3>
-
   <form align="center" action="consultas/consulta1.php" method="post">
     <input type="submit" value="Buscar">
   </form>
   
   <br>
   <br>
-  <br>
 
   <!-- Consulta 2 -->
   <h3 align="center"> Ingrese una comuna. Muestre todos los vehiculos de las unidades ubicadas en esa comuna.</h3>
-
   <form align="center" action="consultas/consulta2.php" method="post">
     Comuna:
     <input type="text" name="comuna_elegida">
+    <br/>
     <input type="submit" value="Buscar">
   </form>
   
   <br>
   <br>
-  <br>
 
 <!-- Consulta 3 -->
   <h3 align="center">Ingrese una comuna y seleccione un año. Muestre todos los vehiculos que hayan realizado un despacho en dicha comuna durante ese año.</h3>
-
   <form align="center" action="consultas/consulta3.php" method="post">
     Comuna:
     <input type="text" name="comuna_elegida">
+    <br/>
     Año:
     <input type="text" name="select_ano">
     <br/>
@@ -45,19 +42,17 @@
   
   <br>
   <br>
-  <br>
 
 <!-- Consulta 4 -->
 <h3 align="center">Ingrese un tipo de vehículo y selecciones dos números. Muestre todos los despachos realizados por un vehiculo del tipo ingresado, y cuyo repartidor tiene una edad entre el rando seleccionado.</h3>
-
 <form align="center" action="consultas/consulta4.php" method="post">
   Tipo Vehiculo:
   <input type="text" name="tipo_vehiculo">
   <br/>
-  Edad 1:
+  Edad Mínima:
   <input type="text" name="edad1">
   <br/>
-  Edad 2:
+  Edad Máxima:
   <input type="text" name="edad2">
   <br/>
   <input type="submit" value="Buscar">
@@ -65,11 +60,9 @@
 
 <br>
 <br>
-<br>
 
 <!-- Consulta 5 -->
 <h3 align="center">Ingrese dos comunas. Encuentre los jefes de las unidades que realizan despachos a ambas comunas.</h3>
-
 <form align="center" action="consultas/consulta5.php" method="post">
   Comuna 1:
   <input type="text" name="comuna1_elegida">
@@ -82,11 +75,9 @@
 
 <br>
 <br>
-<br>
 
 <!-- Consulta 6 -->
 <h3 align="center">Encuentre la unidad que maneja más vehiculos del tipo seleccionado.</h3>
-
 <form align="center" action="consultas/consulta6.php" method="post">
   Tipo Vehiculo:
   <input type="text" name="tipo_vehiculo">
@@ -94,7 +85,6 @@
   <input type="submit" value="Buscar">
 </form>
 
-<br>
 <br>
 <br>
 
